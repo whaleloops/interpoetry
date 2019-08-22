4bff03568800bb4546a325b041347820c5fe6d40
pengshancai <52089867+pengshancai@users.noreply.github.com>
2019-08-21 20:17:12 -0400

    Delete unnecessary comments

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Copyright (c) 2018-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
 
 import os, io
 import sys
