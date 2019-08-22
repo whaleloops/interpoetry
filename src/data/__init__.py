59fe5428ae04ae36e6532d98c7c1577f1d60c609
pengshancai <52089867+pengshancai@users.noreply.github.com>
2019-08-21 20:18:12 -0400

    delete unnecessary comments

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2018-present, Facebook, Inc.
-# All rights reserved.
+
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
